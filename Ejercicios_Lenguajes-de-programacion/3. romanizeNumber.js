2c34a9456a91beb7cfc99e04534dde9fd13c3808
Nekrocow <nekrocow@gmail.com>
2022-12-28 11:26:07 -0300

    refactor romanizeNumber without recursion

--- Ejercicios_Lenguajes-de-programacion/3. romanizeNumber.js
+++ Ejercicios_Lenguajes-de-programacion/3. romanizeNumber.js
@@ -6,53 +6,43 @@ o mayúscula según un modificador que se recibe como parámetro.
 
 
 // al número en decimal, lo chequeamos decrecientemente contra los posibles valores del diccionario romano
-// cuando se cumple, pusheamos la letra correspondiente y restamos la cantidad al número que estamos transformando
-// siempre que el resto > 0, hacemos recursión
+// cuando se cumple, pusheamos la letra correspondiente y inputamos la cantidad al número que estamos transformando
+// siempre que el inputo > 0, hacemos recursión
 
 const { decimalToRomanUpper, decimalToRomanLower } = require('./3. romanizeDictionaries/decimalToRoman') // creamos dos diccionarios correspondientes para números romanos en mayúsculas y en minúsculas (ver archivo -^)
 
-const romanizeNumber = (rest, shouldConvertToUpperCase = true, _latinized = '', _toSubstract = 0) => { // pasamos a la función los argumentos que corresponden a:
-// el número a convertir,
-// si debemos convertir o no a mayúscula (mayúsculas por defecto)
-// y tenemos luego las 2 variables que vamos a utilizar para guardar los valores que utilizará la función:
-// el del número convertido
-// y el de las restas al número a convertir (para chequear si debe continuarse con la recursión)
+const romanizeNumber = (input, shouldConvertToUpperCase = true) => { // pasamos a la función los argumentos que corresponden a:
+  // el número a convertir,
+  // si debemos convertir o no a mayúscula (mayúsculas por defecto)
   const converter = shouldConvertToUpperCase ? decimalToRomanUpper : decimalToRomanLower // la fn elige el diccionario correspondiente
+  let latinized = '' // la usaremos para ir concatenando los números convertidos a romano
+  let toSubstract = 0 // para ir restando los dígitos ya convertidos
 
-  switch (true) { // utilizamos switch por tener mejor performance que if...else; va directamente a la condición que corresponde
-    case (rest <= 0): // caso base 1
-      return "Sorry, can't convert numbers smaller than 1!"
-    case (rest > 9999): // caso base 2
-      return "Sorry, can't convert numbers bigger than 9999!"
-    case (rest > 999):
-      _toSubstract = Math.floor(rest / 1000) * 1000 // aislamos el dígito que queremos convertir
-      _latinized += converter[_toSubstract] // concatenamos al número final el dígito que convertimos
-      break
-    case (rest > 99):
-      _toSubstract += Math.floor(rest / 100) * 100
-      _latinized += converter[_toSubstract]
-      break
-    case (rest > 9):
-      _toSubstract += Math.floor(rest / 10) * 10
-      _latinized += converter[_toSubstract]
-      break
-    default: // si el resto es menor a 10, realizamos la conversión final y retornamos el resultado
-      return _latinized += converter[rest]
+  // chequeamos los dos casos extremos
+  if (input <= 0) return "Sorry, can't convert numbers smaller than 1!"
+  if (input > 9999) return "Sorry, can't convert numbers bigger than 9999!"
+
+  while (input > 0) { // procedemos a convertir
+    // aislamos el dígito que queremos convertir
+    if (input > 999) toSubstract = Math.floor(input / 1000) * 1000
+    else if (input > 99) toSubstract = Math.floor(input / 100) * 100
+    else if (input > 9) toSubstract = Math.floor(input / 10) * 10
+    else toSubstract = input
+    latinized += converter[toSubstract] // concatenamos el dígito que convertimos al resultado en romano
+    input -= toSubstract // reducimos el número original, quitando el dígito ya cubierto
   }
-  if (rest - _toSubstract > 0) { // si aún queda "resto" por convertir, se da lugar a la recursión
-    return romanizeNumber(rest - _toSubstract, shouldConvertToUpperCase, _latinized)
-  } else return _latinized // ... en caso negativo, se retorna el resultado
+  return latinized // devolvemos el número final en romano
 }
 
 // romanizeNumber(-45, true)
 // romanizeNumber(0)
+// romanizeNumber(10000, true)
 // romanizeNumber(1, true)
 // romanizeNumber(2, false)
 // romanizeNumber(9)
+// romanizeNumber(62, false)
 // romanizeNumber(90, false)
 // romanizeNumber(95, false)
-// romanizeNumber(62, false)
 // romanizeNumber(100, true)
 // romanizeNumber(4514, true)
 // romanizeNumber(4532, false)
-// romanizeNumber(10000, true)
